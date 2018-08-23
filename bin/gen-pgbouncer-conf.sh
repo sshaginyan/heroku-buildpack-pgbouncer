d9edc7ff9bc9f361561dc28189d95a84d8a28eed
Steven Shaginyan <sshaginyan@Gmail.com>
2018-08-23 15:29:00 -0700

    Update gen-pgbouncer-conf.sh

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -14,7 +14,7 @@ fi
 cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
 [pgbouncer]
 listen_addr = *
-listen_port = 6000
+listen_port = 5432
 auth_type = md5
 auth_file = /app/vendor/pgbouncer/users.txt
 server_tls_sslmode = prefer
